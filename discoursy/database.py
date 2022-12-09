f4845ff497d54defe96d56194a845f5ca6fc08bb
VincentRPS <vincentbusiness55@gmail.com>
2022-12-09 14:12:01 +0800

    feat: add db and jobs

---
+++ discoursy/database.py
@@ -0,0 +1,105 @@
+from beanie import Document, init_beanie
+from pydantic import Field, BaseModel
+from datetime import datetime
+from typing import Literal
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+async def start_db() -> None:
+    client = AsyncIOMotorClient(os.environ['mongodb_uri'])
+    await init_beanie(
+        client,
+        document_models=[
+            User, Settings, Guild, Member, Invite, Presence, Message, Channel # type: ignore
+        ]
+    )
+
+class User(Document):
+    id: str
+    username: str = Field(min_length=1, max_length=30)
+    discriminator: str = Field(min_length=4, max_length=4)
+    email: str = Field(min_length=5, max_length=15)
+    flags: int
+    system: bool
+    deletor_job_id: str | None
+    suspended: bool
+    pronouns: str = Field('undefined', max_length=10, min_length=1)
+
+class Settings(Document):
+    user_id: str
+    status: Literal['online', 'offline', 'dnd']
+    guild_order: list[int]
+
+class Guild(Document):
+    id: str
+    name: str
+    description: str | None = Field(min_length=1, max_length=1000)
+    flags: int
+    color: int
+    owner_id: str
+    joinable: bool
+    unjoinable_reason: str | None
+
+class Member(Document):
+    user_id: str
+    guild_id: str
+    nick: str
+
+class Invite(Document):
+    code: str
+    guild_id: str
+    author_id: str
+
+class Activity(BaseModel):
+    name: str
+    type: int
+    created_at: datetime
+
+class StatusableActivity(Activity):
+    content: str
+
+class GameActivity(StatusableActivity):
+    game_id: str
+
+class Stream(BaseModel):
+    platform: Literal['youtube', 'twitch']
+    platform_user: str
+    stream_id: str | None
+
+class StreamActivity(StatusableActivity):
+    stream: Stream
+
+class CustomActivity(StatusableActivity):
+    emoji_id: str
+
+class Presence(Document):
+    user_id: str
+    guild_id: str
+    device: Literal['mobile', 'desktop']
+    activities: list[StatusableActivity | GameActivity | StreamActivity | CustomActivity]
+    status: Literal['online', 'offline', 'dnd']
+
+class Message(Document):
+    id: str
+    author_id: str
+    content: str
+    channel_id: str
+    timestamp: datetime
+    edited_timestamp: datetime | None = Field(None)
+
+# Types
+# User-to-User: 0
+# Group: 1
+# Support: 2
+# Text: 3
+# Category: 4
+class Channel(Document):
+    id: str
+    name: str | None
+    last_message_id: str
+    parent_id: str | None
+    guild_id: str | None
+    type: int
+    delete_messages_job_id: str | None = Field()
+    shun: bool = Field(False)
+    members: list[User]
---
+++ discoursy/jobs.py
@@ -0,0 +1,45 @@
+import os
+
+from arq.connections import RedisSettings
+from dotenv import load_dotenv
+from discoursy.database import User, Settings, Presence, Member, Channel, Message
+from typing import Any
+
+load_dotenv()
+
+
+redis_settings = RedisSettings(host=os.environ['redis_host'], port=int(os.environ['redis_port']))
+
+
+async def delete_user(ctx, user_id: str) -> None:
+    await User.find_one(User.id == user_id).delete()
+    await Settings.find_one(Settings.user_id == user_id).delete()
+    await Presence.find(Presence.user_id == user_id).delete()
+    await Member.find(Member.user_id == user_id).delete()
+
+
+async def chunk_guild(ctx, guild_id: str) -> list[dict[str, Any]]:
+    members = []
+    async for member in Member.find(Member.guild_id == guild_id):
+        members.append(member.dict())
+    return members
+
+
+async def delete_channel_messages(ctx, channel_id: str) -> None:
+    channel = await Channel.find_one(Channel.id == channel_id)
+
+    if channel is None:
+        return
+
+    await Message.find(Message.channel_id == channel_id).delete()
+    await channel.delete()
+
+
+async def delete_guild_members(ctx, guild_id: str) -> None:
+    await Member.find(Member.guild_id == guild_id).delete()
+
+
+class WorkerSettings:
+    functions = [delete_user, chunk_guild, delete_channel_messages, delete_guild_members]
+    redis_settings = redis_settings
+    allow_abort_jobs = True
