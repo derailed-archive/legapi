6c224a0c921a289277d576bfef7673c6b8a66c74
VincentRPS <vincentbusiness55@gmail.com>
2022-12-14 18:00:06 +0800

    refactor: add higher backlog to prod

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
-from discoursy.app import app
 import multiprocessing
 
+from discoursy.app import app
+
 if __name__ == '__main__':
-    app.run(workers=multiprocessing.cpu_count(), noisy_exceptions=True)
+    app.run(workers=multiprocessing.cpu_count(), backlog=1000)
